7996704af26aef1ca43bc61f1af8de961301f8cc
PrakulPShetty <shettyprakul6@gmail.com>
2025-12-21 13:13:43 +0530

    pushing the codes

--- Student-Attendance-System/backend/src/main/java/com/attendance/repository/AttendanceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.attendance.repository;
-
-import com.attendance.model.Attendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-}
